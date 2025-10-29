317f8478feae63bf6208611cf50d83fa6a44f478
Yoon Seung Won <85114129+pardax@users.noreply.github.com>
2025-10-29 23:23:14 +0900

    refactor(Enum) : 로또 추첨 방식 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,5 +3,21 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoMachine lotMachine = new LottoMachine();
+
+        //금액 입력
+        lotMachine.GetMoney();
+
+        //당첨번호 입력
+        while(true){
+            System.out.println("당첨 번호를 입력해 주세요.");
+            break;
+        }
+
+        while(true){
+            System.out.println("보너스 번호를 입력해 주세요.");
+            break;
+        }
+        //보너스 입력
     }
 }
