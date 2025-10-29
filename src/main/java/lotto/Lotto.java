f94c3b650fa7dc017592ac3d16c0e5e5e2307425
Yoon Seung Won <85114129+pardax@users.noreply.github.com>
2025-10-29 23:23:51 +0900

    feat(lotto) : 당첨번호 중복 입력 검사 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,13 @@
 package lotto;
 
-import java.util.List;
+import java.util.*;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        findDuplicate(numbers);
         this.numbers = numbers;
     }
 
@@ -17,4 +18,14 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    private void findDuplicate(List<Integer> numbers){
+        Set<Integer> hash = new HashSet<>(numbers);
+        if(numbers.size() != hash.size()){
+            throw new IllegalArgumentException("[ERROR] 중복된 번호는 입력 할 수 없습니다.");
+        }
+    }
+
+    public List<Integer> GetLotto(){
+        return this.numbers;
+    }
 }
